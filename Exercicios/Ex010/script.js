fa491cbaaaacbadf3c6961d983b4241bb02db1f0
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-14 22:30:45 -0300

    Exercicio 10
    resolvido exercicio 10

---
+++ Exercicios/Ex010/script.js
@@ -0,0 +1,17 @@
+let conteudo = document.getElementById('conteudo');
+
+function clicou1() {
+    conteudo.innerHTML = 'Clicou no Botão 1';
+}
+
+function clicou2() {
+    conteudo.innerHTML = 'Clicou no Botão 2';
+}
+
+function clicou3() {
+    conteudo.innerHTML = 'Clicou no Botão 3';
+}
+
+function clicou4() {
+    conteudo.innerHTML = 'Clicou no Botão 4';
+}
